935832a07689377020b9049bad221f349c65e2b9
YuAnKj0 <1614350118@qq.com>
2021-11-29 16:24:55 +0800

    第一次提交，完成SKU+SPU保存

---
+++ changgou_parent/changgou_service/changgou_service_file/src/main/java/com/changgou/file/FileApplication.java
@@ -0,0 +1,20 @@
+package com.changgou.file;
+
+import org.apache.tomcat.jni.File;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author: Ykj
+ * @Description:
+ * @Date: Created in 13:41 2021/11/24
+ * @Modified By:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class);
+    }
+}
---
+++ changgou_parent/changgou_service_api/changgou_service_goods_api/src/main/java/com/changgou/goods/pojo/Goods.java
@@ -0,0 +1,46 @@
+package com.changgou.goods.pojo;
+
+import java.io.PrintStream;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @Author: Ykj
+ * @Description:
+ * @Date: Created in 13:37 2021/11/26
+ * @Modified By:
+ */
+public class Goods implements Serializable {
+    //sku
+    private Spu spu;
+    //SKU集合
+    private List<Sku> skuList;
+
+    public Spu getSpu() {
+        return spu;
+    }
+
+    public void setSpu(Spu spu) {
+        this.spu = spu;
+    }
+
+    public List<Sku> getSkuList() {
+        return skuList;
+    }
+
+    public void setSkuList(List<Sku> skuList) {
+        this.skuList = skuList;
+    }
+
+    @Override
+    public String toString() {
+        return "Goods{" +
+                "spu=" + spu +
+                ", skuList=" + skuList +
+                '}';
+    }
+
+    public List<Sku> getSkus() {
+        return skuList;
+    }
+}
