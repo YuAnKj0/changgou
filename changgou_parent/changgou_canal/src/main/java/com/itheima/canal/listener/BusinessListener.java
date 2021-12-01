f27f87cc8ba3bb218a8f8ec7d50a9fea13cb7704
16143 <1614350118@qq.com>
2021-12-01 23:29:00 +0800

    RabbitMQ拉不下来，烦死了

--- changgou_parent/changgou_canal/src/main/java/com/itheima/canal/listener/BusinessListener.java
+++ changgou_parent/changgou_canal/src/main/java/com/itheima/canal/listener/BusinessListener.java
@@ -1,6 +1,7 @@
 package com.itheima.canal.listener;
 
 import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.itheima.canal.config.RabbitMQConfig;
 import com.xpand.starter.canal.annotation.CanalEventListener;
 import com.xpand.starter.canal.annotation.ListenPoint;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -20,25 +21,16 @@ public class BusinessListener {
      * @param eventType 当前操作数据库的类型
      * @param rowData   当前操作数据库的的数据
      */
-    @ListenPoint(schema = "changgou_business", table = {"tb_ad"})
-    public void adUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
-        System.err.println("广告表数据发生变化");
+    @ListenPoint(schema = "changgou_business",table = "tb_ad")
+    public void adUpdate(CanalEntry.EventType eventType,CanalEntry.RowData rowData){
+        System.out.println("广告表数据发生改变");
 
-        //获取修改前数据
-        for(CanalEntry.Column column: rowData.getBeforeColumnsList()) {
-            if(column.getName().equals("position")){
-                System.out.println("发送消息到mq  ad_update_queue:"+column.getValue());
-                rabbitTemplate.convertAndSend("","ad_update_queue",column.getValue());  //发送消息到mq
-                break;
-            }
-        }
+        for (CanalEntry.Column column : rowData.getAfterColumnsList()) {
+            if ("position".equals(column.getName())){
+                System.out.println("发送最新的数据到MQ:"+column.getValue());
 
-        //修改后数据
-        for(CanalEntry.Column column: rowData.getAfterColumnsList()) {
-            if(column.getName().equals("position")){
-                System.out.println("发送消息到mq  ad_update_queue:"+column.getValue());
-                rabbitTemplate.convertAndSend("","ad_update_queue",column.getValue());  //发送消息到mq
-                break;
+                //发送消息
+                rabbitTemplate.convertAndSend("", RabbitMQConfig.AD_UPDATE_QUEUE,column.getValue());
             }
         }
     }
